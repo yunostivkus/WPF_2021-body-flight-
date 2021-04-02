da89b1ca610e8cd43abefe0853fbcbaab72129cf
yunostivkus <cameramk@mail.ru>
2021-04-02 19:10:36 +0500

    запись в файл

--- body flight/Program.cs
+++ body flight/Program.cs
@@ -1,13 +1,22 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
-namespace Fly_point
+namespace HelloApp
 {
     class Program
     {
-
         static void Main()
         {
+            FlightToFile call = new FlightToFile();
+        }
+    }
+
+    class FlightToFile
+    {
+        public FlightToFile()
+        {
+
             const double G = 9.81;
 
             Console.Write("Введите скорость тела: ");
@@ -15,20 +24,43 @@ namespace Fly_point
             Console.Write("Введите угол: ");
             double corner = Double.Parse(Console.ReadLine());
 
-            Dictionary<double, double> coordinates_point = new Dictionary<double, double>();
+            string writePath = @"C:\Data\GitHub\1.csv";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    Dictionary<double, double> coordinates_point = new Dictionary<double, double>();
 
-            double time = (2 * speed * Math.Sin(corner * Math.PI / 180) / G);
+                    double time = (2 * speed * Math.Sin(corner * Math.PI / 180) / G);
 
-            for (double i = 0; i <= time; i += 0.1)                                                     // записываем округляя до 2 знаков послезапятой
-                coordinates_point.Add(Math.Round((speed * i * Math.Cos(corner * Math.PI / 180)), 2),
-                Math.Round((speed * i * Math.Sin(corner * Math.PI / 180) - G * i * i / 2), 2));
 
-            foreach (var i in coordinates_point)                                        // с помощью цикла foreach выводим координаты точки во время полета
-                Console.WriteLine("Координаты точки в полёте " + i);
+                    for (double i = 0; i <= time; i += 0.1)                                                 // записываем округляя до 2 знаков послезапятой
+                        coordinates_point.Add(Math.Round((speed * i * Math.Cos(corner * Math.PI / 180)), 2),
+                        Math.Round((speed * i * Math.Sin(corner * Math.PI / 180) - G * i * i / 2), 2));
 
-            Console.WriteLine("Время полёта точки: " + Math.Round(time, 2));
-            Console.WriteLine("Общая протяженность пути: " + Math.Round((speed * speed *
-            Math.Sin(2 * corner * Math.PI / 180)) / G), 2);
+
+                    for (double dt = 0; dt < time; dt += 0.1)               // вывод времени 
+                    {
+                        sw.WriteLine("time: " + Math.Round(dt, 2));
+                    }
+
+                    foreach (var i in coordinates_point)                     // с помощью цикла foreach выводим координаты точки во время полета
+                    {
+                        sw.WriteLine("body coordinates: " + i + " ");
+                    }
+
+                    sw.WriteLine("Time in flight: " + Math.Round(time, 2));
+                    sw.WriteLine("The length of the path: " + Math.Round((speed * speed *
+                    Math.Sin(2 * corner * Math.PI / 180)) / G), 2);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
+
+
     }
 }
+
