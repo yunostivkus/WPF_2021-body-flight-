d90ee6210f161f0ee12920c78b089d85b5583203
yunostivkus <cameramk@mail.ru>
2021-04-23 21:44:57 +0500

    Переделан Метод Эйлера, добавлен обработчик

--- body flight/Program.cs
+++ body flight/Program.cs
@@ -1,64 +1,97 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
 
-namespace HelloApp
+namespace body_flight
 {
-    class Program
+    class Flight
     {
-        static void Main()
+        private double G = 9.81;
+        double speed, corner, body_mass;
+
+        public Flight(double spe, double corn, double mass)
         {
-            FlightToFile call = new FlightToFile();
+            speed = spe;
+            corner = (Math.PI * corn) / 180;
+            body_mass = mass;
         }
+
+        public double y_coefficient_for_calculation(double time)
+        {
+            return 4 * Math.Cos(1.5 * time);
+        }
+        public double x_coefficient_for_calculation(double time)
+        {
+            return 3.4 * Math.Cos(1.9 * time);
+        }
+
+        public void Сalculation()
+        {
+            double speed_x = speed * Math.Cos(corner);
+            double speed_y = speed * Math.Sin(corner);
+            double x_coordinate = 0, y_coordinate = 0, t = 0.01;
+
+            Console.WriteLine(x_coordinate + "  --  " + y_coordinate);
+
+            do
+            {
+                x_coordinate = x_coordinate + (0.01 * speed_x);
+                y_coordinate = y_coordinate + (0.01 * speed_y);
+
+                if (y_coordinate < 0)
+                    y_coordinate = 0;
+
+                speed_x = velocity_x(t, speed_x, body_mass, 0.01);
+                speed_y = velocity_y(t, speed_x, body_mass, 0.01);
+
+                Console.WriteLine(x_coordinate + "  --  " + y_coordinate);
+
+                t += 0.01;
+            }
+            while (y_coordinate > 0);
+        }
+
+        
+        public double velocity_x(double time, double speed, double mass, double delta)
+        {
+            return speed - (delta * x_coefficient_for_calculation(time) * speed / mass);
+        }
+        public double velocity_y(double time, double speed, double mass, double delta)
+        {
+            return speed - ((G + (delta * y_coefficient_for_calculation(time) * speed)) / mass);
+        }
+
     }
 
-    class FlightToFile
+    class Program
     {
-        public FlightToFile()
+        static void Main(string[] args)
         {
-
-            const double G = 9.81;
-
-            Console.Write("Введите скорость тела: ");
-            double speed = Double.Parse(Console.ReadLine());
-            Console.Write("Введите угол: ");
-            double corner = Double.Parse(Console.ReadLine());
-
-            string writePath = @"C:\Data\GitHub\1.csv";
             try
             {
-                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                Console.Write("Введите скорость тела: ");
+                double speed = Double.Parse(Console.ReadLine());
+                Console.Write("Введите угол: ");
+                double corner = Double.Parse(Console.ReadLine());
+                Console.Write("Введите массу тела: ");
+                double mass = Double.Parse(Console.ReadLine());
+
+
+                if (speed <= 0 || corner >= 90 || mass <= 0)
                 {
-                    Dictionary<double, double> coordinates_point = new Dictionary<double, double>();
-
-                    double time = (2 * speed * Math.Sin(corner * Math.PI / 180) / G);
-
-
-                    for (double i = 0; i <= time; i += 0.1)                                                 // записываем округляя до 2 знаков послезапятой
-                        coordinates_point.Add(Math.Round((speed * i * Math.Cos(corner * Math.PI / 180)), 2),
-                        Math.Round((speed * i * Math.Sin(corner * Math.PI / 180) - G * i * i / 2), 2));
-
-
-                    for (double dt = 0; dt < time; dt += 0.1)               // вывод времени 
-                    {
-                        sw.WriteLine("time: " + Math.Round(dt, 2));
-                    }
-
-                    foreach (var i in coordinates_point)                     // с помощью цикла foreach выводим координаты точки во время полета
-                    {
-                        sw.WriteLine("body coordinates: " + i + " ");
-                    }
-
-                    sw.WriteLine("Time in flight: " + Math.Round(time, 2));
-                    sw.WriteLine("The length of the path: " + Math.Round((speed * speed *
-                    Math.Sin(2 * corner * Math.PI / 180)) / G), 2);
+                    Console.WriteLine("Неверно указаны величины");
+                    return;
                 }
+
+                Flight Body = new Flight(speed, corner, mass);
+             
+                Body.Сalculation();
+
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
+                Console.WriteLine("Неверные данные");
             }
         }
     }
 }
-
